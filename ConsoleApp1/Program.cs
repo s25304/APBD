88a2b3e97d0424c7a8fa08168cf37c841bdc2337
s25304 <s25304@pjwstk.edu.pl>
2024-03-15 00:00:40 +0100

    add average

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.IO.IsolatedStorage;
+
 Console.WriteLine("Hello, World!");
 //mod1
 //mod2
 //mod3
+static double average(int[] src)
+{
+    int sum = 0;
+    for(int i =0;i < src.Length; i++)
+    {
+        sum += src[i];
+    }
+    return (double) sum / src.Length;
+}
